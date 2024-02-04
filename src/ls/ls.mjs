09b442bbb9098ac2e0ce848227e4d5876435bf68
Jacek <j.klosinski@ibe.edu.pl>
2024-02-04 19:25:57 +0100

    fix: ls sort

--- src/ls/ls.mjs
+++ src/ls/ls.mjs
@@ -1,12 +1,22 @@
 
 import { readdir } from "node:fs/promises";
 
+const sortFiles = (a, b) => {
+    if(a.type === 'directory' && b.type === 'file') {
+        return -1;
+    } else if (a.type === 'file' && b.type === 'directory') {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
 export const list = async (path) => {
   const files = await readdir(path, { withFileTypes: true });
   console.table(
     files.map((file) => ({
       Name: file.name,
       type: file.isDirectory() ? "directory" : "file",
-    }))
-  );
+    })).sort((a, b) => sortFiles(a, b))
+    )
 };
