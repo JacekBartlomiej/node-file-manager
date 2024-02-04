619db92e36aaa55471c2677359518ec4d0a646df
Jacek <j.klosinski@ibe.edu.pl>
2024-02-04 15:57:07 +0100

    feat: app stdin process and exit

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,27 @@
 import { getUserName } from "./username/username.mjs";
+import process from "node:process";
 
 let __username = "Unknown";
 
 const init = () => {
   __username = getUserName();
   console.log(`Welcome to the File Manager, ${__username}`);
+  console.log("Please print your command and wait for result...");
+  process.on("SIGINT", () => {
+    process.exit();
+  });
+  process.on("exit", () => {
+    console.log(`Thank you for using File Manager, ${__username}, goodbye!`);
+  });
+  process.stdin.on("data", (data) => {
+    const exitCommand = ".exit";
+    const command = data.toString().trim();
+    if (command === exitCommand) {
+      process.exit();
+    } else {
+      console.log(`You printed: ${data}`);
+    }
+  });
 };
 
 init();
